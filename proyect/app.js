4dbd82f7f81c7ae0d21906e4f4d5a454ef73b903
Tom <tomvanderlaan10@gmail.com>
2020-05-18 10:29:54 -0300

    registro con ruta

--- proyect/app.js
+++ proyect/app.js
@@ -22,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', homeRouter);
 app.use('/users', usersRouter);
-app.use('/products', productsRouter)
+app.use('/products', productsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
