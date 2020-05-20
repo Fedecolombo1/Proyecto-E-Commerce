1c6c47c04a8f7d57202e2f1bc2d8dae87df95dc1
Tom <tomvanderlaan10@gmail.com>
2020-05-20 15:13:40 -0300

    unos cambios al log in y agregue el carrito

--- proyect/routes/products.js
+++ proyect/routes/products.js
@@ -11,6 +11,8 @@ router.get('/detail', function(req, res, next) {
 router.get('/add', function(req, res, next) {
   res.render('productAdd');
 });
-
+router.get('/cart', function(req, res, next) {
+  res.render('productCart');
+});
 
 module.exports = router;
