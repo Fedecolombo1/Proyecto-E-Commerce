99776e30521ca81b9b0e4d695a5225b405e40f1a
Fedecolombo1 <colombofederico17@gmail.com>
2020-06-16 17:32:47 -0300

    cree login middleware

---
+++ proyect/middlewares/usersMiddleware.js
@@ -0,0 +1,41 @@
+const bcrypt = require("bcrypt")
+const fs = require("fs")
+
+var users = JSON.parse(fs.readFileSync("./database/users.json"))
+
+var middleware = {
+    login: function(req, res, next){
+        pass = bcrypt.hashSync("hola" , 10)
+        console.log(pass);
+        /*  for(var i = 0; i < users.length; i++){
+            if(bcrypt.compareSync(req.body.password, users[i].password) && req.body.email == user[i].email)){
+                req.session.usuario = "logueado"
+                next();
+            } else {res.render("Login")}     
+          }   */
+
+        if(req.body.password == "hola" && req.body.email == "2fede14@gmail.com"){
+            req.session.usuario = "logueado"
+            next();
+        } else {res.render("Login")}    
+        
+        console.log(req.body.recuerdame);
+        
+
+        if(req.body.recuerdame == on){
+            res.cookie('fede', '123', {maxAge: 3600000})
+        } else {
+            next()
+        }
+          
+    },
+    auth: function(req, res, next){
+        if(req.session.usuario != undefined){
+            next()
+        } else {
+            res.redirect("/users/login")
+        }
+    }
+}
+
+module.exports = middleware
